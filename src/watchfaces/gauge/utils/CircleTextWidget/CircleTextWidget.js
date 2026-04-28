780d526b80011e337a96bb479d14979273d569c3
NicolasRoelofs <57498304+NicolasRoelofs@users.noreply.github.com>
2026-04-28 13:31:07 +0200

    Update CircleTextWidget.js

--- src/watchfaces/gauge/utils/CircleTextWidget/CircleTextWidget.js
+++ src/watchfaces/gauge/utils/CircleTextWidget/CircleTextWidget.js
@@ -1,5 +1,5 @@
-import { TEXT_CHAR_HEIGHT, TEXT_CHAR_WIDTH, TEXT_CHAR_WIDTHS, TEXT_CHARS } from './chars';
-import { calculateAnglularLength, radiansToDegrees } from './utils';
+import { TEXT_CHAR_HEIGHT, TEXT_CHAR_WIDTH, TEXT_CHARS } from './chars';
+import { calculateAnglularLength } from './utils';
 
 /**
  * Circle text, where every char is hmUI.widget.IMG widget
@@ -11,88 +11,100 @@ export class CircleTextWidget {
     this._charImages = TEXT_CHARS;
     this._imageWidth = TEXT_CHAR_WIDTH;
     this._imageHeight = TEXT_CHAR_HEIGHT;
-
     this._screenSize = px(480);
-
     this._maxLength = maxLength;
-
     this._kerningPairs = kerningPairs;
 
-    this._chars = text
-      .toLowerCase()
-      .slice(0, maxLength)
-      .padEnd(maxLength, ' ')
-      .split('');
+    this._angleStart = angleStart;
+    this._radius = radius;
+    this._gap = gap;
+    this._isTextReversed = isTextReversed;
 
-    this._widgets = this._createWidgets({
-      angleStart,
-      radius,
-      gap,
-      isTextReversed,
+    this._chars = this._normalizeText(text);
+
+    this._widgets = this._createWidgets();
+  }
+
+  _normalizeText(text) {
+    return text
+      .toLowerCase()
+      .slice(0, this._maxLength)
+      .padEnd(this._maxLength, ' ')
+      .split('');
+  }
+
+  _calculateAngles(chars) {
+    let angleStart = this._angleStart;
+
+    if (this._isTextReversed) {
+      angleStart = -1 * (180 - angleStart);
+    }
+
+    const imageAngle = calculateAnglularLength(this._radius, this._imageWidth);
+
+    let gapAngle = calculateAnglularLength(this._radius, this._gap);
+
+    if (this._gap < 0) {
+      gapAngle *= -1;
+    }
+
+    let extraAngle = 0;
+
+    return chars.map((char, i) => {
+      if (i > 0) {
+        const pair = `${chars[i - 1]}${char}`;
+        const extraGap = this._kerningPairs[pair] || 0;
+
+        if (extraGap) {
+          extraAngle += calculateAnglularLength(this._radius, px(extraGap));
+        }
+      }
+
+      return this._isTextReversed
+        ? angleStart - i * imageAngle - i * gapAngle - extraAngle
+        : angleStart + i * imageAngle + i * gapAngle + extraAngle;
     });
   }
 
-  _createWidgets({ angleStart, radius, gap, isTextReversed }) {
-    if (isTextReversed) {
-      angleStart = -1 * (180 - angleStart);
-    }
-  
-    const imageAngle = calculateAnglularLength(radius, this._imageWidth);
-  
-    let gapAngle = calculateAnglularLength(radius, gap);
-  
-    if (gap < 0) {
-      gapAngle *= -1;
-    }
-  
-    let extraAngle = 0;
-  
-    return this._chars.map((char, i) => {
-      if (i > 0) {
-        const pair = `${this._chars[i - 1]}${char}`;
-        const extraGap = this._kerningPairs[pair] || 0;
-  
-        if (extraGap) {
-          extraAngle += calculateAnglularLength(radius, px(extraGap));
-        }
-      }
-  
-      return hmUI.createWidget(hmUI.widget.IMG, {
+  _createWidgets() {
+    const angles = this._calculateAngles(this._chars);
+
+    return this._chars.map((char, i) =>
+      hmUI.createWidget(hmUI.widget.IMG, {
         src: this._charImages[char] || this._charImages[' '],
         w: this._screenSize,
         h: this._screenSize,
         x: 0,
         y: 0,
         pos_x: this._screenSize / 2 - this._imageWidth / 2,
-        pos_y: isTextReversed
-          ? this._screenSize / 2 + radius
-          : this._screenSize / 2 - radius - this._imageHeight,
+        pos_y: this._isTextReversed
+          ? this._screenSize / 2 + this._radius
+          : this._screenSize / 2 - this._radius - this._imageHeight,
         center_x: this._screenSize / 2,
         center_y: this._screenSize / 2,
-        angle: isTextReversed
-          ? angleStart - i * imageAngle - i * gapAngle - extraAngle
-          : angleStart + i * imageAngle + i * gapAngle + extraAngle,
+        angle: angles[i],
         show_level: hmUI.show_level.ONLY_NORMAL,
-      });
-    });
+      }),
+    );
   }
-  
+
   updateText(text) {
-    const newTextChars = text
-      .toLowerCase()
-      .slice(0, this._maxLength)
-      .padEnd(this._maxLength, ' ')
-      .split('');
+    const newTextChars = this._normalizeText(text);
+    const newAngles = this._calculateAngles(newTextChars);
 
     for (let i = 0; i < this._chars.length; i++) {
-      if (this._chars[i] === newTextChars[i]) continue;
+      if (this._chars[i] !== newTextChars[i]) {
+        this._widgets[i].setProperty(
+          hmUI.prop.SRC,
+          this._charImages[newTextChars[i]] || this._charImages[' '],
+        );
 
-      this._widgets[i].setProperty(
-        hmUI.prop.SRC,
-        this._charImages[newTextChars[i]] || this._charImages[' '],
-      );
+        this._chars[i] = newTextChars[i];
+      }
 
-      this._chars[i] = newTextChars[i];
+      this._widgets[i].setProperty(hmUI.prop.MORE, {
+        angle: newAngles[i],
+      });
     }
   }
 }
