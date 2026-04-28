ff6216411fe844ffeac78bc47f93bf04a92ee0a6
NicolasRoelofs <57498304+NicolasRoelofs@users.noreply.github.com>
2026-04-28 09:45:03 +0200

    Update constants.js

--- src/watchfaces/gauge/utils/constants.js
+++ src/watchfaces/gauge/utils/constants.js
@@ -28,7 +28,7 @@ export const HEART_TEXT = isRusLang
 export const SLEEP_TEXT = isRusLang
   ? 'СОН %s'
   : isFrLang
-    ? 'SOM\u200AMEIL %s'
+    ? 'SOMMEIL %s'
     : 'SLEEP %s'
 
 const STEPS_POSTFIX_RU = ['шаг', 'шага', 'шагов']
