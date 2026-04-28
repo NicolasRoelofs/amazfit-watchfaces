fc6399d139ab8e5c9ef3b2168d3b6f4232aacc97
NicolasRoelofs <57498304+NicolasRoelofs@users.noreply.github.com>
2026-04-28 13:05:12 +0200

    Update index.js

--- src/watchfaces/gauge/watchface/index.js
+++ src/watchfaces/gauge/watchface/index.js
@@ -136,9 +136,9 @@ WatchFace({
       gap: px(-3),
       isTextReversed: true,
       kerningPairs: {
-        om: -0.5,
-        mm: 1,
-        me: -0.5,
+        om: 1,
+        mm: 1.5,
+        me: 1,
       },
     });
     const sleepSensor = hmSensor.createSensor(hmSensor.id.SLEEP);
