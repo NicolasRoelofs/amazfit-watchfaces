1725fcfd48f3db0c35f8f312a4ea5195eb3621f1
NicolasRoelofs <57498304+NicolasRoelofs@users.noreply.github.com>
2026-07-21 13:59:27 +0200

    Add files via upload

---
+++ src/adapters/getTimeTexts.js
@@ -0,0 +1,18 @@
+import { getHourNoLeadingZeroText, getHourText, getMinuteText, getPostfixText } from '../utils/time';
+import { getIs12HourFormat } from './getIs12HourFormat';
+
+/**
+ * Gets current time string values
+ * @param {HmSensorInstance} timeSensor
+ */
+export function getTimeTexts(timeSensor) {
+  const { hour = 0, minute = 0 } = timeSensor;
+  const is12HourFormat = getIs12HourFormat();
+
+  return {
+    hourText: getHourText(hour, is12HourFormat),
+    hourNoLeadingZeroText: getHourNoLeadingZeroText(hour, is12HourFormat),
+    minuteText: getMinuteText(minute),
+    postfixText: getPostfixText(hour, is12HourFormat),
+  };
+}
